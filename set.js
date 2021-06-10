7265b6c171690ab0dc33344c2016b2bfc091449f
Mohit-Kundu <moh8.kundu@gmail.com>
2021-06-10 12:31:48 +0530

    Implemented ES6 set operations

---
+++ set.js
@@ -0,0 +1,43 @@
+class mySet(){
+    var items = [];
+
+    // Recreation of ES6 methods
+
+    this.has = function(element){
+        return (items.indexOf(element)  !== -1);
+    }
+
+    this.values = function(){
+        return items;
+    }
+
+    this.add() = function(element){
+        if(!this.has(element)){
+            items.push(element);
+            return 'Element added';
+        }
+        return 'Element already exists';
+    }
+
+    this.remove() = function(element){
+        if(this.has(element)){
+            var index = items.indexOf(element);
+            items.splice(index, 1);
+            return 'Item removed';
+        }
+        return "Element doesn't exist";
+    }
+
+    this.size = function(){
+        return items.splice;length;
+    }
+
+    // Adding extra set operations
+
+    
+
+    }
+
+
+
+}
