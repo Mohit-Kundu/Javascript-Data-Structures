14be8588cb69507d938f3a92a969c51ccc85da97
Mohit-Kundu <moh8.kundu@gmail.com>
2021-06-24 22:06:13 +0530

    Added inorder, preorder and postorder functions

--- bst.js
+++ bst.js
@@ -136,6 +136,35 @@ class BST{
             return node;
         }
     }
+
+    //DFS functions
+
+    // left, root, right
+    inorder(node){
+        if(node !== null){
+            this.inorder(node.left);
+            console.log(node.data);
+            this.inorder(node.right);
+        }
+    }
+
+    //root, left, right
+    preorder(node){
+        if(node !== null){
+            console.log(node.data);
+            this.preorder(node.left);
+            this.preorder(node.right);
+        }
+    }
+
+    //left, right, root
+    postorder(node){
+        if(node !== null){
+            this.postorder(node.left);
+            this.postorder(node.right);
+            console.log(node.data);
+        }
+    }
 }
 
 
